4bb7af9a3e7f06a9f721c6bfbcd959885519c2b8
Jim Sanders <j.sanders@student.fontys.nl>
2015-06-13 23:30:44 +0200

    Added category loading from database

--- Individuele Opdracht Bax/Individuele Opdracht Bax/index.aspx.cs
+++ Individuele Opdracht Bax/Individuele Opdracht Bax/index.aspx.cs
@@ -12,22 +12,53 @@ namespace Individuele_Opdracht_Bax
         protected void Page_Load(object sender, EventArgs e)
         {
             
+            LoadCategories();
         }
 
+        /// <summary>
+        /// Redirect to displayed website the login webpage.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Redirect_Login(object sender, EventArgs e)
         {
             Response.Redirect("Login.aspx");
         }
 
+        /// <summary>
+        /// Test method. This method adds a test user control to the index webpage.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Add_UserControl(object sender, EventArgs e)
         {
             var uc = (WebUserControl1)Page.LoadControl("test.ascx");
-
-
-            
-
             innerContent.Controls.Add(uc);
             
         }
+
+        private void LoadCategories()
+        {
+            var con = DbProvider.GetOracleConnection();
+            var com = con.CreateCommand();
+            
+            com.CommandText =
+                                @"SELECT naam,plaatje
+                                FROM CATEGORIE
+                                WHERE BehoortTot = 'Gitaar'";
+            
+            var r = com.ExecuteReader();
+
+            while (r.Read())
+            {
+                var uc = (Category)Page.LoadControl("Category.ascx");
+
+                uc.Name = (string) r["naam"];
+                uc.ImageLink = (string) r["plaatje"];
+            
+                innerContent.Controls.Add(uc);
+                uc.LoadData();
+            }
+        }
     }
-}
+    }
